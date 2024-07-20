66b218f2cfe7c2c0920402b8dc2618dd2df3f354
jackcooperusesvim <andreft316@gmail.com>
2024-07-19 22:11:17 -0400

    almost done with initial version

--- .ipynb_checkpoints/eda-checkpoint.py
+++ .ipynb_checkpoints/eda-checkpoint.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 import config 
 import sqlite3 as sql
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 def import_filt(plot = False):
     data = pd.read_csv(config.CSV_INPUT_FILEPATH())
@@ -11,20 +13,15 @@ def import_filt(plot = False):
 def filt(data, plot = False):
     # Remove everything with afk time
     #Make the charStats managable
+
     data["missed"] = data.charStats.apply(extract_missed)
     data["extra"] = data.charStats.apply(extract_extra_letters)
     data["incorrect"] = data.charStats.apply(extract_incorrect)
     data["ind"] = pd.Series(data.index)
+    data["rawWpm"] = data["wpm"]/data["rawWpm"]
+
     if plot:
-        #Ordered data according to the order in which the tests were taken
-        fig, ax = plt.subplots()
-        ax.set_label("wpm by id")
-        ax.scatter(data.index, data.wpm)
-        #
-        # fig2, ax2 = plt.subplots()
-        # ax2.scatter(data.timestamp[::-1], data.wpm[::-1])
-        # ax2.set_label("wpm by time")
-        plt.show()
+        sns.relplot(data = data, x = "ind", y = "wpm").set(title="Before")
 
 
     # this section filters out the Colemak days and non-representative datapoints and outliers
@@ -33,30 +30,28 @@ def filt(data, plot = False):
 
     m,b = line_from_points2d(370,70,500,20)
 
-    # != is the same as xor
-    colemak_filter = (data.wpm<data.ind*m+b) & (data.wpm<70) & (300<data.ind)
-    outlier_filter = (300>data.ind) & (data.wpm<70)
-    old_filter = (360<data.ind) & (data.wpm>(data.ind*m+b))
-    
+    colemak_filter = (data.timestamp>1.7*(10**12)) & (data.wpm<data.ind*m+b) & (data.wpm<70) & (300<data.ind)
+    outlier_filter = ((300>data.ind) & (data.wpm<70)) | (data.acc<80)
+    col_qwert_filter = (data.timestamp>1.7*(10**12)) & (data.timestamp<1.715*(10**12)) & ~colemak_filter
+    old_filter = (360<data.ind) & (data.wpm>(data.ind*m+b)) & ~col_qwert_filter
+
     colemak_data = data[colemak_filter]
-    old_data = data[old_filter]
-    new_data = data[~colemak_filter & ~old_filter & ~outlier_filter]
+    col_qwert_data = data[col_qwert_filter]
+    old_data = data[old_filter & ~col_qwert_filter]
+    new_data = data[~colemak_filter & ~col_qwert_filter & ~old_filter & ~outlier_filter]
     #old_data = data.where(data.old_data).dropna(how="all")
     #colemak_data = data.where(data.colemak).dropna(how="all")
-    #The following lines show the filtering
-    if plot:
-        plt.scatter(colemak_data.ind, colemak_data.wpm)
-        plt.scatter(old_data.ind, old_data.wpm)
-        plt.scatter(new_data.ind, new_data.wpm)
-        #plt.scatter(np.array(data.index), data.wpm, c = data.old_data)
-        plt.plot(data.ind,data.ind*m+b)
-        plt.show()
 
     new_data = new_data.assign(dataGroup = "new")
     old_data =  old_data.assign(dataGroup = "old")
     colemak_data = colemak_data.assign(dataGroup = "colemak")
+    col_qwert_data= col_qwert_data.assign(dataGroup = "col_qwert")
+
+    out_data = pd.concat([old_data,colemak_data,new_data,col_qwert_data], axis = 0) 
     
-    return pd.concat([old_data,colemak_data,new_data], axis = 0)
+    if plot:
+        sns.relplot(out_data,y="wpm",x="ind",hue='dataGroup').set(title="After")
+    return out_data
 
 
 def line_from_points2d(x1,y1,x2,y2):
