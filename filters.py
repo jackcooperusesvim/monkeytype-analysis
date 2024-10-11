76108d188188bbc0557865923e7f7e274a0a798f
jackcooperusesvim <andreft316@gmail.com>
2024-10-11 15:05:48 -0400

    fri aft

--- filters.py
+++ filters.py
@@ -3,35 +3,34 @@ from icecream import ic
 import polars as pl
 import seaborn as sns
 
-def import_filt(dg = 'new'):
+def import_filt(*dataGroups):
     data = pl.read_csv(config.CSV_INPUT_FILEPATH(),infer_schema_length=int(1e10))
-    x = filt(data,dg)
+
+    if len(dataGroups)==0: 
+        dataGroups = ['new']
+
+    x = filt(data,dataGroups)
     print(x.columns)
     return x
 
-def filt(data: pl.DataFrame,dg = 'new'):
+def filt(data: pl.DataFrame, dataGroups: list[str] = ['new']):
 
-    # Declare Filter Line
     m,b = line_from_points2d(200,50,300,80)
 
     return data.with_columns(
-        correct = pl.col("charStats").str.split(";").list.get(0).str.to_integer(),
-        incorrect = pl.col("charStats").str.split(";").list.get(1).str.to_integer(),
-        extra = pl.col("charStats").str.split(";").list.get(2).str.to_integer(),
-        missed = pl.col("charStats").str.split(";").list.get(3).str.to_integer(),
-        timeIndex= pl.len()-pl.Series(range(1,len(data)+1))
-
+        timeIndex = pl.len()-pl.Series(range(1,len(data)+1)),
     ).with_columns(
-        testLen = pl.col("correct")+pl.col("incorrect")+pl.col("missed"),
-        incorrect_prop = pl.col('correct')/pl.col('incorrect'),
-        missed_prop = pl.col('correct')/pl.col('missed'),
-        extra_prop = pl.col('correct')/pl.col('extra')
-    ).filter(
-        #Filter out all 
-        pl.col('quoteLength') == -1,
-        pl.col('afkDuration') == 0
-
-    ).with_columns(
+        testType = (
+            pl.when(pl.col('testDuration') <= 29).then(
+                pl.lit('short')
+            ).when(pl.col('testDuration') == 30).then(
+                pl.lit('long')
+            ).when(pl.col('quoteLength') != -1).then(
+                pl.lit('quote')
+            ).otherwise(
+                pl.lit('other')
+            )
+        ),
         dataGroup = (
             pl.when(pl.col("timestamp") > 1.715*(10**12)).then(
                     pl.when(pl.col('wpm')<70).then(
@@ -44,21 +43,47 @@ def filt(data: pl.DataFrame,dg = 'new'):
             ).when(pl.col('timestamp') < 1.7*(10**12)).then(
                 pl.lit('old')
             ).when(pl.col("timeIndex")*m+b > pl.col('wpm')).then(
-                pl.lit('qwert_col')
-            ).otherwise(
                 pl.lit('colemak')
+            ).otherwise(
+                pl.lit('qwert_col')
             )
         ),
-        isQuote = pl.col('quoteLength') != -1,
-
+        correct = pl.col("charStats").str.split(";").list.get(0).str.to_integer(),
+        incorrect = pl.col("charStats").str.split(";").list.get(1).str.to_integer(),
+        extra = pl.col("charStats").str.split(";").list.get(2).str.to_integer(),
+        missed = pl.col("charStats").str.split(";").list.get(3).str.to_integer(),
     ).filter(
-        pl.col("dataGroup")==dg,
-        pl.col('afkDuration')==0,
+        pl.col('testType') != pl.lit('quote'),
+        pl.col('afkDuration') == 0,
+        pl.col("dataGroup").is_in(dataGroups),
         pl.col('wpm') is not pl.Null,
-        pl.col('testLen') < 400
-    ).with_columns(
-        testIsShort = pl.col('testDuration') < 18
     )
+def graph_ops(data: pl.DataFrame, select:bool=False) -> pl.DataFrame:
+    if select:
+        return data.select(
+            restartCountAdjusted = pl.col('restartCount').log1p(),
+            oldAvgWpm = pl.col('wpm')-pl.col('wpm').where(pl.col('dataGroup')=='old').mean(),
+            newAvgWpm = pl.col('wpm')-pl.col('wpm').where(pl.col('dataGroup')=='new').mean(),
+            colemakAvgWpm = pl.col('wpm')-pl.col('wpm').where(pl.col('dataGroup')=='colemak').mean(),
+            qwert_colAvgWpm = pl.col('wpm')-pl.col('wpm').where(pl.col('dataGroup')=='qwert_col').mean(),
+        )
+    else:
+        return data.with_columns(
+            restartCountAdjusted = pl.col('restartCount').log1p(),
+            AvgWpmComparison = (
+                    pl.when(pl.col('dataGroup') == 'old').then(
+                        pl.col('wpm')-pl.col('wpm').where(pl.col('dataGroup')=='old').mean(),
+                    ).when(pl.col('dataGroup') == 'new').then(
+                        pl.col('wpm')-pl.col('wpm').where(pl.col('dataGroup')=='new').mean(),
+                    ).when(pl.col('dataGroup') == 'colemak').then(
+                        pl.col('wpm')-pl.col('wpm').where(pl.col('dataGroup')=='colemak').mean(),
+                    ).when(pl.col('dataGroup') == 'qwert_col').then(
+                        pl.col('wpm')-pl.col('wpm').where(pl.col('dataGroup')=='qwert_col').mean(),
+                    ).otherwise(
+                    0
+                    )
+            ),
+        )
 
 
 def line_from_points2d(x1,y1,x2,y2):
---
+++ util.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib
+import sklearn.linear_model as lm
+
+def linreg(data:pd.DataFrame,x:str,y:str,ax:matplotlib.axes.Axes|None =None,color:str|None=None,label:str|None=None):
+    X = [0,max(data[[x]])]
+    model = lm.LinearRegression().fit(data[[x]],data[[y]])
+    Y = model.predict(X=X)
+
+    return sns.regplot(x=X,y=Y,data=data,color=color,label=label,ax=ax)
